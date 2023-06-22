ae7b9c2ae737dce5c05cde794565b07c376d089d
justlikeazoo <125628057+justlikeazoo@users.noreply.github.com>
2023-06-22 09:59:51 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.py
@@ -0,0 +1,29 @@
+def solution(date1, date2):
+    # date1의 연도, 월, 일을 각각 변수에 저장합니다.
+    year1, month1, day1 = date1
+    
+    # date2의 연도, 월, 일을 각각 변수에 저장합니다.
+    year2, month2, day2 = date2
+    
+    # date1이 date2보다 앞서는지 비교합니다.
+    if year1 < year2:
+        return 1
+    elif year1 > year2:
+        return 0
+    else:
+        if month1 < month2:
+            return 1
+        elif month1 > month2:
+            return 0
+        else:
+            if day1 < day2:
+                return 1
+            elif day1 > day2:
+                return 0
+            else:
+                return 0  # date1과 date2가 동일한 경우
+
+# 예시 테스트케이스
+print(solution([2022, 1, 1], [2023, 1, 1]))  # 1
+print(solution([2023, 1, 1], [2022, 1, 1]))  # 0
+print(solution([2023, 1, 1], [2023, 1, 1]))  # 0
